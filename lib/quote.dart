9a471429669f19b5526155d5d0f2026f84dc4fdb
Dima Antonenko <daemondw@gmail.com>
2020-09-26 00:18:42 +0200

    changes added

--- lib/quote.dart
+++ lib/quote.dart
@@ -1,6 +1,25 @@
 class Quote {
-  String author;
-  String text;
 
-  Quote({this.author, this.text});
+  String text;
+  String author;
+
+  //  normal constructor, as we've already seen
+
+  //  Quote(String author, String text){
+  //    this.text = text;
+  //    this.author = author;
+  //  }
+
+  //  constructor with named parameters
+
+  //  Quote({ String author, String text }){
+  //    this.text = text;
+  //    this.author = author;
+  //  }
+
+  // constructor with named parameters
+  // & automatically assigns named arguments to class properties
+
+  Quote({ this.text, this.author });
+
 }
